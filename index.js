46cf849f500b53c2ce417937eeb5755fceb68ca9
minhnhat <phancaominhnhat10@gmail.com>
2021-05-19 22:21:21 +0700

    Boostrap 5

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const {join} = require('path');
+const morgan = require('morgan');
+const { static } = require('express');
 const app = express();
 
 const port = 3000;
+console.log(__dirname);
+//tạo một đường dẫn url tĩnh của thư mục public ta làm như sau : 
+app.use(express.static(join(__dirname,"public")));
+app.set('views',join(__dirname,'views'));
+
+//use template engine pub
+app.set('view engine', 'pug');
+
+//HTTP logger
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-    let a = 1;
-    let b = 2;
-    let c = a + b;
-    return res.send("hello");
+    return res.render('pages/home.pug',{title: "pUB", message : "hello"});
 })
 
 app.listen(3000);
